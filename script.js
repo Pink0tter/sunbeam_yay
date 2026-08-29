aed81e3a38a804c4c526380226f09c37ebc4aa03
Pink0tter <fawcett204@gmail.com>
2026-08-29 23:20:27 +0000

    Complete

--- script.js
+++ script.js
@@ -1,8 +1,224 @@
-function clickButton(){
-    alert("You have pressed a button!");
-    console.log("Button pressed");
+function Getoutofbed(){
+    alert("Go to number 2")
 }
 
-function clicked(){
-    alert("Go to ...")
+function Gobacktosleep(){
+    alert("GET OUT OF BED YOU LAZY BUM!!! Now go to number 2")
+}
+
+function Eatbreakfast(){
+    alert("Go to number 3")
+}
+
+function Skipbreakfast(){
+    alert("Go to number 4")
+}
+
+function MemyselfandI(){
+    alert("Go to number 5")
+}
+
+function one(){
+    alert("Go to number 5")
+}
+
+function two(){
+    alert("Go to number 5")
+}
+
+function threeplus(){
+    alert("Go to number 5")
+}
+
+function pancakes(){
+    alert("Yum! Now go to number 4")
+}
+
+function waffles(){
+    alert("Yum! Now go to number 4")
+}
+
+function fruit(){
+    alert("Yum! Now go to number 4")
+}
+
+function cereal(){
+    alert("Yum! Now go to number 4")
+}
+
+function candy(){
+    alert("Yum! Now go to number 4")
+}
+
+function stayhome(){
+    alert("You are too boring. Go outside and touch some grass. Back yet? Now go choose another option from number 5.")
+}
+
+function amusementpark(){
+    alert("Go to number 10")
+}
+
+function outofworld(){
+    alert("Go to number 6")
+}
+
+function planets(){
+    alert("Go to number 7")
+}
+
+function universe(){
+    alert("Go to number 14")
+}
+
+function rocky(){
+    alert("Go to number 9")
+}
+
+function gas(){
+    alert("Go to number 8")
+}
+function land(){
+    alert("You died because its not possible to land on a gas planet. JK you get another life go pick another option at number 5")
+}
+
+function fly(){
+    alert("That was such a beautiful view! You can now go to another place(number 5), the gas planets(number 7), another universe(number 6), or go home(number 13).")
+}
+
+function land1(){
+    alert("You got a great view of the universe and had fun exploring. You can now go to another place(number 5), the gas planets(number 7), another universe(number 6), or go home(number 13).")
+}
+
+function fly1(){
+    alert("You had fun observing the planet. You can now go to another place(number 5), the gas planets(number 7), another universe(number 6), or go home(number 13).")
+}
+
+function getfood(){
+    alert("Go to number 11")
+}
+
+function ride(){
+    alert("Go to number 19.")
+}
+
+function game(){
+    alert("Go to number 20")
+}
+
+function churro(){
+    alert("Yum! Go to number 12")
+}
+
+function funnelcake(){
+    alert("Yum! Go to number 12")
+}
+
+function pretzel(){
+    alert("Yum! Go to number 12")
+}
+
+function icecream(){
+    alert("Yum! Go to number 12")
+}
+
+function hotdog(){
+    alert("Yum! Go to number 12")
+}
+
+function gohome(){
+    alert("You still have time, if you want to go somewhere else, go to number 5, otherwise you can go to number 13.")
+}
+
+function alien(){
+    alert("Go to number 15")
+}
+
+function cat(){
+    alert("Go to number 16")
+}
+
+function dog(){
+    alert("Go to number 17")
+}
+
+function talk(){
+    alert("He says, 'Vorp? Beegle boogle bop bogos binted?' Then he gets angry and banishes you from the alien universe >:D")
+}
+
+function attack(){
+    alert("He attacks you back >:D Now you are dead.")
+}
+
+function run(){
+    alert("The alien runs way faster than you. It catches you and you are tortured to death in front of other aliens. >:D")
+}
+
+function run1(){
+    alert("You survive. If you want to go to another universe, go to number 14. Otherwise, you can go to number 13.")
+}
+
+function talk1(){
+    alert("The cat says, 'Meow'. You can now choose another option(number 16), go to another universe(number 14), go somewhere else(number 5), or go home(number 13).")
+}
+
+function attack1(){
+    alert("The cat scratches you to death. >:D")
+}
+
+function yes(){
+    alert("We hope you enjoyed your perfect day!")
+}
+
+function no(){
+    alert("THINK AGAIN...")
+}
+
+function run2(){
+    alert("You barely made it off the planet. Go back to number 17 to make another choice, or go home by going to number 13.")
+}
+
+function attack2(){
+    alert("The dog makes a feast off your body...[this means you are dead]")
+}
+
+function talk2(){
+    alert("The dog says 'woof woof'. Go back to number 17 to make another choice, or go home by going to number 13. You can also go someplace else(number 5) or another universe(number 14).")
+}
+
+function nah(){
+    alert("Fine then. To go to another universe, go to number 14. To go someplace else, go to number 5. To go home, go to number 13.")
+}
+
+function sure(){
+    alert("You adopt the cat with no paperwork! To go home, go to number 13. To go to another universe, go to number 14. To go someplace else, go to number 5.")
+}
+
+function rollercoaster(){
+    alert("Didn't you like the thrill of that rollercoaster? To go on another ride(number 19), go somewhere else(number 5), or go home(number 13).")
+}
+
+
+function carousel(){
+    alert("That was so fun! To go on another ride(number 19), go somewhere else(number 5), or go home(number 13).")
+}
+
+
+function bumpcar(){
+    alert("You got bumped a little too many times, but at least you're still alive... You can go on another ride(number 19), go somewhere else(number 5), or go home(number 13).")
+}
+
+function droptower(){
+    alert("Wasn't it so scary? You can go on another ride(number 19), go somewhere else(number 5), or go home(number 13).")
+}
+
+function claw(){
+    alert("You got a plushie! You can go play another game(number 20), go somewhere else(number 5), or go home(number 13).")
+}
+
+function arcgame(){
+    alert("You got a new high score! You can go play another game(number 20), go somewhere else(number 5), or go home(number 13).")
+}
+
+function goback(){
+    alert("You're so boring, you just got here! Go choose another option from number 12.")
 }
